47f384f183fbdb544fb457f73d852304106e546a
Nadia Mitterer <nadia.mitterer@yahoo.it>
2025-12-02 11:22:26 +0100

    finished day 2

--- days/day2.py
+++ days/day2.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import time
 
@@ -16,31 +17,30 @@ start_time = time.time()
 input_data = input_data[0].split(',')
 id_ranges = [[line.split('-')[0], line.split('-')[1]] for line in input_data]
 
-# part 1
+
+
 result_part_1 = 0
+result_part_2 =  0
+
+# found regex patterns here: https://www.reddit.com/r/regex/comments/3devu1/how_to_match_on_repeated_substrings/
+regex_pattern1 =  r'^(\d*)\1$'
+regex_pattern2 = r'^(\d*)\1+$'
 
 for range_pair in id_ranges:
     start, end = range_pair[0], range_pair[1]
     
     # check the whole id range for invalid IDs
     for curr_id in range(int(start), int(end) + 1):
-        string_id = str(curr_id)
-        id_len = len(string_id)
-        
-        # only even length IDs can be checked for invalidity
-        if id_len % 2 != 0:
-            continue
-        
-        # check if the first half is in the second half -> repeated twice
-        half_len = id_len // 2
-        if string_id[: half_len] in string_id[half_len:]:
+        # regex check if substring repeats exactly twice
+        if re.match(regex_pattern1, str(curr_id)):
             result_part_1 += curr_id
-
-# part 2
-result_part_2 =  0
+        
+        # regex check if substring repeats more than twice
+        if re.match(regex_pattern2, str(curr_id)):
+            result_part_2 += curr_id
 
 # print the results
 print(f"--- Day {day}: ---")
 print(f"Part 1: {result_part_1}") # 12599655151
-print(f"Part 2: {result_part_2}")
+print(f"Part 2: {result_part_2}") # 20942028255
 print(f"Duration: {time.time() - start_time} seconds")
